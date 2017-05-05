0acfd8fc9b3677055a3a92ee9a0a059ae0cd5579
sbower <shawn.bower@gmail.com>
2017-05-05 11:17:39 -0400

    changes per review

--- scripts/ctbTrain.js
+++ scripts/ctbTrain.js
@@ -3,20 +3,17 @@ const AWS = require('aws-sdk');
 const piSQS = process.env.PI_QUEUE;
 const SQS = new AWS.SQS({region: 'us-east-1'});
 
-module.exports = function(robot) {
-  robot.hear(/ctb train/i, function(msg) {
+module.exports = (robot) => {
+  robot.hear(/ctb train/i, (msg) => {
 
-    var message_payload = { action: 'play_sound', sound: 'train' };
+    const message_payload = { action: 'play_sound', sound: 'train' };
 
     SQS.sendMessage({
-       MessageBody:  JSON.stringify(message_payload),
+       MessageBody: JSON.stringify(message_payload),
        QueueUrl: piSQS
-    }).promise().then(function(data) {
-      console.log('Heard CTB Train and queued message');
-    }).catch(function(err) {
-      console.log(err);
-    });
-
-
+    })
+    .promise()
+    .then((data) => { console.log('Heard CTB Train and queued message'); })
+    .catch((err) => { console.log(err); });
   })
 }
